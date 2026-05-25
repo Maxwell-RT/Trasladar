0074f667d7ab473bfbb64396df8639d420588fb1
Maxwell-RT <claud.munozs@duocuc.cl>
2026-05-25 00:05:14 -0400

    first commit

---
+++ src/main/java/com/trabajo/Trasladar/TrasladarApplication.java
@@ -0,0 +1,13 @@
+package com.trabajo.Trasladar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasladarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrasladarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabajo/Trasladar/TrasladarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabajo.Trasladar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrasladarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
