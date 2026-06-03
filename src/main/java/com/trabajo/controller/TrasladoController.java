25fa78bb6a2085c481d664557d5a9f9896a47843
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-06-03 14:15:59 -0400

    archivados

--- src/main/java/com/trabajo/controller/TrasladoController.java
+++ src/main/java/com/trabajo/controller/TrasladoController.java
@@ -2,6 +2,7 @@ package com.trabajo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,20 +24,20 @@ public TrasladoController(TrasladoService trasladoService) {
     }
 
 
-@PatchMapping("/{id}/aprobar")
-    public ResponseEntity<Traslado> aprobar(@PathVariable Long id) {
-        return ResponseEntity.ok(trasladoService.aprobar(id));
-    }
+    @GetMapping("/{id}/aprobar")
+        public ResponseEntity<Traslado> aprobar(@PathVariable Long id) {
+            return ResponseEntity.ok(trasladoService.aprobar(id));
+        }
 
-@PatchMapping("/{id}/rechazar")
-    public ResponseEntity<Traslado> rechazar(
-            @PathVariable Long id,
-            @RequestParam String motivo) {
+    @GetMapping("/{id}/rechazar")
+        public ResponseEntity<Traslado> rechazar(
+                @PathVariable Long id,
+                @RequestParam String motivo) {
 
-        return ResponseEntity.ok(trasladoService.rechazar(id, motivo));
-    }
+            return ResponseEntity.ok(trasladoService.rechazar(id, motivo));
+        }
 
-    @PatchMapping("/{id}/cancelar")
+    @GetMapping("/{id}/cancelar")
     public ResponseEntity<Traslado> cancelar(@PathVariable Long id) {
         return ResponseEntity.ok(trasladoService.cancelar(id));
     }
