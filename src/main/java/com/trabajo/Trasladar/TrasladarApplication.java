a3915f7b6934eb4bc3b255afef1e86f567948e07
Maxwell-RT <claud.munozs@duocuc.cl>
2026-06-24 19:17:06 -0400

    "Fixed almost"

--- src/main/java/com/trabajo/Trasladar/TrasladarApplication.java
+++ src/main/java/com/trabajo/Trasladar/TrasladarApplication.java
@@ -3,7 +3,8 @@ package com.trabajo.Trasladar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = org.springdoc.core.configuration.SpringDocHateoasConfiguration.class)
 public class TrasladarApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/trabajo/Trasladar/TrasladarApplicationTests.java
+++ src/test/java/com/trabajo/Trasladar/TrasladarApplicationTests.java
@@ -1,13 +1,23 @@
 package com.trabajo.Trasladar;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TrasladarApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	void mainClassExist(){
+		assertNotNull(TrasladarApplication.class);
+	}
+
+
 
 }
